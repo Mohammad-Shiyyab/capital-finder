dc62933c3cb5025f8ad822dcfe20d4a01ff5811a
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-06-12 18:51:31 +0000

    city

--- api/capital.py
+++ api/capital.py
@@ -14,6 +14,7 @@ class handler(BaseHTTPRequestHandler):
     url_components = parse.urlsplit(url_path)
     query_list = parse.parse_qsl(url_components.query)
     my_dict = dict(query_list)
+    name=my_dict['name']
 
     # print(111,my_dict)
     if 'capital' in my_dict:
@@ -22,11 +23,19 @@ class handler(BaseHTTPRequestHandler):
       res = requests.get(url+capital)
       data = res.json()
       cuntry=data[0]["name"]["common"]
+      messaag=f'the cuntry of {capital} is {cuntry}'
 
     #   print(222,data)
-  
-
-    self.wfile.write(str(cuntry).encode())
+    if 'cuntry' in my_dict:
+        cuntry = my_dict.get('cuntry')
+        url= 'https://restcountries.com/v3.1/name/'
+        res = requests.get(url+cuntry)
+        data = res.json()
+        capital=data[0]["capital"]
+        messaag=f' {capital} is the capital of {cuntry}'
+    
+      
+    self.wfile.write(str(messaag).encode())
     return
 
 
