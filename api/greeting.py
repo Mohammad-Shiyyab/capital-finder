212a404ac2a4c2d32066c52218fcafd42302282b
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-06-12 17:46:52 +0000

    cap

---
+++ api/greeting.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests 
+class handler(BaseHTTPRequestHandler):
+ 
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_list = parse.parse_qsl(url_components.query)
+    my_dict = dict(query_list)
+
+    # print(111,my_dict)
+    if 'capital' in my_dict:
+      capital = my_dict.get('capital')
+      url= 'https://restcountries.com/v3.1/capital/'
+      res = requests.get(url+capital)
+      data = res.json()
+    #   print(222,data)
+
+    
+
+
+
+
+    self.wfile.write(capital.encode())
+    return
