fe0fd7e02a5f425e2de0c79c687a758daeb8d128
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-06-12 18:03:19 +0000

    cap

--- api/capital.py
+++ api/capital.py
@@ -1,11 +1,32 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
  
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests 
 class handler(BaseHTTPRequestHandler):
  
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_list = parse.parse_qsl(url_components.query)
+    my_dict = dict(query_list)
+
+    # print(111,my_dict)
+    if 'capital' in my_dict:
+      capital = my_dict.get('capital')
+      url= 'https://restcountries.com/v3.1/capital/'
+      res = requests.get(url+capital)
+      data = res.json()
+      cuntry=data[0]["name"]["common"]
+
+    #   print(222,data)
+  
+
+    self.wfile.write(str(cuntry).encode())
     return
+
+
